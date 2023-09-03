08314052aa2c4accbbb2fa941651b68d84e27976
sojeongw <sojeongw.official@gmail.com>
2023-09-03 18:18:04 +0900

    ch01.p02: 반복되지 않는 첫 번쨰 문자 찾기 - 필요없는 conversion을 제거하고 최적화한다

--- src/main/java/org/examples/chapter01/p02_firstnonrepeatedcharacter/Main.java
+++ src/main/java/org/examples/chapter01/p02_firstnonrepeatedcharacter/Main.java
@@ -1,23 +1,25 @@
 package org.examples.chapter01.p02_firstnonrepeatedcharacter;
 
-import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("문자열을 입력하세요.");
+
         Scanner scanner = new Scanner(System.in);
         String input = scanner.next();
 
-        Character key = input.chars()
-                .mapToObj(i -> (char) i)
-                .collect(Collectors.groupingBy(value -> value, Collectors.counting()))
-                .entrySet().stream()
-                .filter(e -> e.getValue() == 1)
-                .findFirst()
-                .orElseThrow(NoSuchElementException::new).getKey();
+        int[] ascii = new int[256];
 
-        System.out.println("key = " + key);
+        for (char c : input.toCharArray()) {
+            ascii[c]++;
+        }
+
+        for (char c : input.toCharArray()) {
+            if (ascii[c] == 1) {
+                System.out.println("result = " + c);
+                return;
+            }
+        }
     }
 }

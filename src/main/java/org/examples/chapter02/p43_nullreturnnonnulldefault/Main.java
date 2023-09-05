3c821c1cebf993d7bb5e95cbfe9d06bdd3d974e4
sojeongw <sojeongw.official@gmail.com>
2023-09-05 20:34:44 +0900

    ch02.p43: null 참조 검사와 null이 아닌 기본 참조 반환 - requireNonNullElse 사용

--- src/main/java/org/examples/chapter02/p43_nullreturnnonnulldefault/Main.java
+++ src/main/java/org/examples/chapter02/p43_nullreturnnonnulldefault/Main.java
@@ -3,12 +3,10 @@ package org.examples.chapter02.p43_nullreturnnonnulldefault;
 import org.examples.chapter02.p41_nullthenthrowcustomnpe.Laptop;
 
 import java.util.Objects;
-import java.util.Optional;
 
 public class Main {
 
     private static Laptop checkNull(Laptop object) {
-        return Optional.of(object)
-                .orElse(new Laptop(100, "mac"));
+        return Objects.requireNonNullElse(object, new Laptop(1000, "mac"));
     }
 }
